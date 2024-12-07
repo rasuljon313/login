c91bff75985cdf00a80f6627707c756c6fa52424
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-07 22:52:39 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { Button, Form, Input } from "antd";
 
-export default App
+const App = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    form.resetFields();
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form form={form} name="basic" labelCol={{span: 8,}} wrapperCol={{ span: 16,}} style={{  maxWidth: 600, }} initialValues={{remember: true,}}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off" className="form">
+      <Form.Item placeholder="username" name="username" rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}>
+        <Input className="username" placeholder="Enter your Username" />
+      </Form.Item>
+      <Form.Item
+        name="password" 
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password className="password" placeholder="Enter your password" />
+      </Form.Item>
+      <Form.Item label={null}>
+        <Button className="btn" type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default App;
+
