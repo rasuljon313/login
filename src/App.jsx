c49a887d1a1203e602623ba2288d22c443994444
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-09 00:16:52 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,63 @@
+// import { Button, Form, Input } from "antd";
+// import toast from "react-hot-toast";
+// const App = () => {
+//   const [form] = Form.useForm();
+
+//   const onFinish = (values) => {
+//     console.log("Success:", values);
+//     toast.success("Muvaffaqiyatli otdingiz");
+//     form.resetFields();
+//   };
+
+//   const onFinishFailed = (errorInfo) => {
+//     console.log("Failed:", errorInfo);
+//   };
+
+//   return (
+//     <Form form={form} name="basic" labelCol={{span: 8,}} wrapperCol={{ span: 16,}} style={{  maxWidth: 600, }} initialValues={{remember: true}}
+//       onFinish={onFinish}
+//       onFinishFailed={onFinishFailed}
+//       autoComplete="off" className="form">
+//       <Form.Item placeholder="username" label="Username" name="username" rules={[
+//           {
+//             required: true,
+//             message: "Please input your username!",   
+//           },
+//           {
+//             min: 5,
+//             message: "Username must be at least 5 characters long!",
+//           },
+//         ]}>
+//         <Input className="username" placeholder="Enter your Username" />
+//       </Form.Item>
+//       <Form.Item label="Password" name="password" rules={[
+//           { 
+//             required: true,
+//             message: "Please input your password!",
+//           },
+//           {
+//             min: 5,
+//             message: "Username must be at least 5 characters long!",
+//           }
+//         ]}
+//       >
+//         <Input.Password className="password" placeholder="Enter your password" />
+//       </Form.Item>
+//       <Form.Item label={null}>
+//         <Button className="btn" type="primary" htmlType="submit">
+//           Submit
+//         </Button>
+//       </Form.Item>
+//     </Form>
+//   );
+// };
+
+// export default App;
+
+
 import { Button, Form, Input } from "antd";
 import toast from "react-hot-toast";
+
 const App = () => {
   const [form] = Form.useForm();
 
@@ -14,34 +72,56 @@ const App = () => {
   };
 
   return (
-    <Form form={form} name="basic" labelCol={{span: 8,}} wrapperCol={{ span: 16,}} style={{  maxWidth: 600, }} initialValues={{remember: true}}
+    <Form
+      form={form}
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
-      autoComplete="off" className="form">
-      <Form.Item placeholder="username" label="Username" name="username" rules={[
+      autoComplete="off"
+      className="form"
+    >
+      <Form.Item
+        label="Tell number"
+        name="phone"
+        rules={[
           {
             required: true,
-            message: "Please input your username!",   
+            message: "Please input your phone number!",
           },
           {
-            min: 5,
-            message: "Username must be at least 5 characters long!",
+            pattern: /^\+998[1-9][0-9]{8}$/,
+            message: "Phone number must be in the format +998XXXXXXXXX!",
           },
-        ]}>
-        <Input className="username" placeholder="Enter your Username" />
+        ]}
+      >
+        <Input
+          className="phone"
+          placeholder="Enter your phone number"
+          type="tel"
+        />
       </Form.Item>
-      <Form.Item label="Password" name="password" rules={[
-          { 
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
             required: true,
             message: "Please input your password!",
           },
           {
             min: 5,
-            message: "Username must be at least 5 characters long!",
-          }
+            message: "Password must be at least 5 characters long!",
+          },
         ]}
       >
-        <Input.Password className="password" placeholder="Enter your password" />
+        <Input.Password
+          className="password"
+          placeholder="Enter your password"
+        />
       </Form.Item>
       <Form.Item label={null}>
         <Button className="btn" type="primary" htmlType="submit">
