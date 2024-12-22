a143bbe71d5b45f150706f2c6ac2893ee391f46b
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-22 23:08:19 +0500

    add

--- src/components/Cars/Cars.jsx
+++ src/components/Cars/Cars.jsx
@@ -1,76 +1,3 @@
-// import Nav from "../../components/nav/Nav"
-// import Sidebar from "../../components/bar/Sidebar"
-// import { useEffect, useState } from "react"
-// import CModal from "../modal/CModal"
-// const Cars = () => {
-//   const [saveBrand, setSaveBrand] = useState([])
-//   const [saveCars, setSaveCars] = useState([])
-//   const [openC, setOpenC] = useState(false)
-//   const [color, setColor] = useState("")
-//   const [brandID, setBrandId] = useState("")
-//   const token = localStorage.getItem("tokenxon");
-//   const formdata = new FormData()
-//   formdata.append("color", color)
-//   formdata.append("brand_id", brandID)
-
-//   const getBrand = () => {
-//     fetch("https://realauto.limsa.uz/api/brands")
-//     .then((res) => res.json())
-//     .then((elem) => setSaveBrand(elem?.data))
-//   }
-//   const getCar = () => {
-//     fetch("https://realauto.limsa.uz/api/cars")
-//     .then((res) => res.json())
-//     .then((elem) => setSaveCars(elem?.data))
-//   }
-//   const pushApiC = () => {
-//     fetch("https://realauto.limsa.uz/api/cars",{
-//       method:"POST",
-//       headers:{
-//         Authorization: `Bearer ${token}`
-//       },
-//       body:formdata
-//     })
-//     .then((elem) => {
-//       if(elem.success){
-//         getCar()
-//         setOpenC(false)
-//         setColor("")
-//       }
-//     })
-//   }
-
-//   // console.log(saveBrand);
-//   console.log(saveCars);
-  
-//   useEffect(()=>{
-//     getBrand()
-//     getCar()
-//   },[])
-//   return (
-//     <div>
-
-//      <Nav setOpenC={setOpenC} />
-//      <div className="cars">
-//      <Sidebar/>
-//       <div className="cars_box">
-//         <div className="container">
-//           <ul className="cars_list">
-//            <li className="cars_item">Brand name</li>
-//            <li className="cars_item">Color</li>
-//           </ul>
-//         </div>
-//       </div>
-//      </div>
-//      {
-//       openC && <CModal pushApiC={pushApiC} setBrandId={setBrandId} setColor={setColor} setOpenC={setOpenC} saveBrand={saveBrand} />
-//      }
-//     </div>
-//   )
-// }
-
-// export default Cars
-
 import Nav from "../../components/nav/Nav";
 import Sidebar from "../../components/bar/Sidebar";
 import { useEffect, useState } from "react";
@@ -79,9 +6,34 @@ import CModal from "../modal/CModal";
 const Cars = () => {
   const [saveBrand, setSaveBrand] = useState([]);
   const [saveCars, setSaveCars] = useState([]);
+  const [saveModels, setSaveModels] = useState([]);
+  const [saveCity, setSaveCity] = useState([]);
+  const [location, setLocation] = useState([]);
   const [openC, setOpenC] = useState(false);
   const [color, setColor] = useState("");
+  const [year, setYear] = useState("");
+  const [imgID, setImg] = useState("");
+  const [maxspeedID, setMaxspeed] = useState("");
+  const [maxpeopleID, setMaxpeople] = useState("");
+  const [transmissionID, setTransmission] = useState("");
+  const [setMatorID, setMator] = useState("");
+  const [driveSide, setDrives] = useState("");
+  const [second, setSecond] = useState("");
+  const [petrol, setPetrol] = useState("");
+  const [limitperday, setLimitp] = useState("");
+  const [deposit, setDeposit] = useState("");
+  const [premiump, setPremiumP] = useState("");
+  const [premiuma, setPremiumA] = useState("");
+  const [premiumAS, setPremiumAS] = useState("");
+  const [premiumu, setPremiumU] = useState("");
+  const [premiumUS, setPremiumUS] = useState("");
+  const [inclusive, setInclusive] = useState("");
+  const [cover, setCover] = useState("");
+
   const [brandID, setBrandId] = useState("");
+  const [modelID, setModelID] = useState("");
+  const [cityID, setCityID] = useState("");
+  const [locationID, setLocationID] = useState("");
   const token = localStorage.getItem("tokenxon");
 
   const getBrand = () => {
@@ -89,36 +41,90 @@ const Cars = () => {
       .then((res) => res.json())
       .then((elem) => setSaveBrand(elem?.data));
   };
-
+  const getModel = () => {
+    fetch("https://realauto.limsa.uz/api/models")
+      .then((res) => res.json())
+      .then((elem) => setSaveModels(elem?.data));
+  };
+  const getCity = () => {
+    fetch("https://realauto.limsa.uz/api/cities")
+      .then((res) => res.json())
+      .then((elem) => setSaveCity(elem?.data));
+  };
+  const getLocation = () => {
+    fetch("https://realauto.limsa.uz/api/locations")
+      .then((res) => res.json())
+      .then((elem) => setLocation(elem?.data));
+  };
   const getCar = () => {
     fetch("https://realauto.limsa.uz/api/cars")
       .then((res) => res.json())
       .then((elem) => setSaveCars(elem?.data));
   };
-
-  const pushApiC = () => {
-    const formdata = new FormData();
-    formdata.append("color", color);
-    formdata.append("brand_id", brandID);
-
-    fetch("https://realauto.limsa.uz/api/cars", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      data: formdata,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data)
-      })
-      .catch((err) => console.error("Error:", err));
-  };
-
+  
   useEffect(() => {
     getBrand();
     getCar();
+    getModel();
+    getCity();
+    getLocation();
   }, []);
+  
+  const pushApiC = (e) => {
+    e.preventDefault();
+    const formdata = new FormData();
+    formdata.append("color", color);
+    formdata.append("brand_id", brandID);
+    formdata.append("location_id", locationID);
+    formdata.append("model_id", modelID);
+    formdata.append("city_id", cityID);
+    formdata.append("year", year);
+    formdata.append("seconds", second);
+    formdata.append("max_speed", maxspeedID);
+    formdata.append("max_people", maxpeopleID);
+    formdata.append("transmission", transmissionID);
+    formdata.append("motor", setMatorID);
+    formdata.append("drive_side", driveSide);
+    formdata.append("petrol", petrol);
+    formdata.append("limitperday", limitperday);
+    formdata.append("deposit", deposit);
+    formdata.append("premium_protection", premiump);
+    formdata.append("price_in_aed", premiuma);
+    formdata.append("price_in_usd", premiumu);
+    formdata.append("price_in_aed_sale", premiumAS);
+    formdata.append("price_in_usd_sale", premiumUS);
+    formdata.append("inclusive", inclusive);
+    formdata.append("cover", cover);
+  if(imgID)formdata.append("images[]", imgID);
+
+    fetch("https://realauto.limsa.uz/api/cars",{
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        // "Content-Type": "application/json; charset=utf-8",
+      },
+      body: formdata,
+    })
+    .then((res) => res.json())
+    .then((elem) => console.log(elem))
+      // .then((res) => res.json())
+      // .then((data) => {
+      //   if (data.success) {
+      //     getCar();
+      //     setOpenC(false);
+      //     setColor("");
+      //     setYear("");
+      //     setSecond("");
+      //     setCityID("");
+      //     setModelID("");
+      //     setBrandId("");
+      //     setImg(null);
+      //   } else {
+      //     console.error("Error:", data.message);
+      //   }
+      // })
+      // .catch((err) => console.error("Error:", err));
+  };
 
   return (
     <div>
@@ -131,19 +137,41 @@ const Cars = () => {
               <li className="cars_item">Brand name</li>
               <li className="cars_item">Color</li>
             </ul>
-            {
-              saveCars?.map((item) => (
-                <section key={item.id}>
-                  <div className="cars_color">{item.color}</div>
-                  <div className="cars_color">{item.brand.image_src}</div>
-                </section>
-              ))
-            }
+            {saveCars?.map((item) => (
+              <section key={item.id}>
+                <div className="cars_color">{item.color}</div>
+                <div className="cars_color">{item.brand.image_src}</div>
+              </section>
+            ))}
           </div>
         </div>
       </div>
       {openC && (
         <CModal
+          setCover={setCover}
+          setInclusive={setInclusive}
+          setLocationID={setLocationID}
+          location={location}
+          setPremiumUS={setPremiumUS}
+          setPremiumAS={setPremiumAS}
+          setPremiumU={setPremiumU}
+          setPremiumA={setPremiumA}
+          setPremiumP={setPremiumP}
+          setDeposit={setDeposit}
+          setLimitp={setLimitp}
+          setPetrol={setPetrol}
+          setDrives={setDrives}
+          setMator={setMator}
+          setTransmission={setTransmission}
+          setMaxspeed={setMaxspeed}
+          setMaxpeople={setMaxpeople}
+          setImg={setImg}
+          setSecond={setSecond}
+          setYear={setYear}
+          setCityID={setCityID}
+          saveCity={saveCity}
+          setModelID={setModelID}
+          saveModels={saveModels}
           pushApiC={pushApiC}
           setBrandId={setBrandId}
           setColor={setColor}
@@ -157,3 +185,4 @@ const Cars = () => {
 };
 
 export default Cars;
+
--- src/components/modal/CModal.jsx
+++ src/components/modal/CModal.jsx
@@ -1,12 +1,38 @@
 /* eslint-disable react/prop-types */
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
-const CModal = ({ setOpenC, pushApiC, saveBrand, setBrandId, setColor }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    pushApiC(); 
-    setOpenC(false); 
-  };
+const CModal = ({ 
+  setOpenC, 
+  pushApiC, 
+  setYear, 
+  setMaxspeed, 
+  location, 
+  setCover, 
+  setInclusive, 
+  setLocationID, 
+  setPremiumP, 
+  setPremiumA, 
+  setPremiumAS, 
+  setPremiumUS,  
+  setPremiumU, 
+  setDeposit, 
+  setPetrol, 
+  setLimitp, 
+  setDrives, 
+  setMator, 
+  setTransmission, 
+  setMaxpeople, 
+  saveModels, 
+  setSecond, 
+  setImg, 
+  setCityID, 
+  saveCity, 
+  saveBrand, 
+  setBrandId, 
+  setModelID, 
+  setColor 
+}) => {
+
 
   return (
     <div>
@@ -15,29 +41,67 @@ const CModal = ({ setOpenC, pushApiC, saveBrand, setBrandId, setColor }) => {
           <button className="modal_close" onClick={() => setOpenC(false)}>
             <IoIosCloseCircleOutline />
           </button>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={pushApiC}>
             <h2 className="modal_title">{"Add Model"}</h2>
-            <input
-              required
-              onChange={(e) => setColor(e.target.value)}
-              type="text"
-              placeholder="Enter car color"
-            />
-            <select
-              name="chose"
-              onChange={(e) => setBrandId(e.target.value)}
-              aria-label="Choose a brand"
-              required
-            >
-              <option value="" disabled>
-                Select a brand
-              </option>
-              {saveBrand?.map((item) => (
-                <option key={item.id} value={item.id}>
-                  {item.title}
-                </option>
-              ))}
-            </select>
+            <div className="modal_box">
+
+              <select name="chose" onChange={(e) => setBrandId(e.target.value)} aria-label="Choose a brand" required>
+                <option value="" disabled selected>Select a brand</option>
+                {saveBrand?.map((item) => (
+                  <option key={item.id} value={item.id}>
+                    {item.title}
+                  </option>
+                ))}
+              </select>
+
+              <select name="chose" onChange={(e) => setModelID(e.target.value)} aria-label="Choose a model" required>
+                <option value="" disabled selected>Select a model</option>
+                {saveModels?.map((item) => (
+                  <option key={item.id} value={item.id}>
+                    {item.brand_title}
+                  </option>
+                ))}
+              </select>
+
+              <select name="chose" onChange={(e) => setCityID(e.target.value)} aria-label="Choose a city" required>
+                <option value="" disabled selected>Select a city</option>
+                {saveCity?.map((item) => (
+                  <option key={item.id} value={item.id}>
+                    {item.name}
+                  </option>
+                ))}
+              </select>
+
+              <select name="chose" onChange={(e) => setLocationID(e.target.value)} aria-label="Choose a location" required>
+                <option value="" disabled selected>Select a location</option>
+                {location?.map((item) => (
+                  <option key={item.id} value={item.id}>
+                    {item.name}
+                  </option>
+                ))}
+              </select>
+
+              <input required onChange={(e) => setColor(e.target.value)} type="text" placeholder="Car color"/>
+              <input required onChange={(e) => setInclusive(e.target.value)} type="text" placeholder="Inclusive"/>
+              <input required onChange={(e) => setCover(e.target.value)} type="text" placeholder="Cover"/>
+              <input required onChange={(e) => setYear(e.target.value)} type="number" placeholder="Car year"/>
+              <input required onChange={(e) => setSecond(e.target.value)} type="number" placeholder="Car seconds"/>
+              <input required onChange={(e) => setMaxspeed(e.target.value)} type="number" placeholder="Max speed"/>
+              <input required onChange={(e) => setMaxpeople(e.target.value)} type="number" placeholder="Max people"/>
+              <input required onChange={(e) => setTransmission(e.target.value)} type="number" placeholder="Transmission"/>
+              <input required onChange={(e) => setMator(e.target.value)} type="number" placeholder="Motor"/>
+              <input required onChange={(e) => setDrives(e.target.value)} type="number" placeholder="Drive side"/>
+              <input required onChange={(e) => setPetrol(e.target.value)} type="number" placeholder="Petrol"/>
+              <input required onChange={(e) => setLimitp(e.target.value)} type="number" placeholder="Limit"/>
+              <input required onChange={(e) => setDeposit(e.target.value)} type="number" placeholder="Deposit"/>
+              <input required onChange={(e) => setPremiumP(e.target.value)} type="number" placeholder="Premium"/>
+              <input required onChange={(e) => setPremiumA(e.target.value)} type="number" placeholder="Premium AED"/>
+              <input required onChange={(e) => setPremiumU(e.target.value)} type="number" placeholder="Premium USD"/>
+              <input required onChange={(e) => setPremiumAS(e.target.value)} type="number" placeholder="AED sale"/>
+              <input required onChange={(e) => setPremiumUS(e.target.value)} type="number" placeholder="USD sale"/>
+              
+              <input type="file" accept="image/*" multiple required onChange={(e) => setImg(e.target.files)} />
+            </div>
             <button type="submit">Submit</button>
           </form>
         </div>
@@ -47,3 +111,4 @@ const CModal = ({ setOpenC, pushApiC, saveBrand, setBrandId, setColor }) => {
 };
 
 export default CModal;
+
